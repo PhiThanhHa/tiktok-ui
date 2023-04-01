7df4482cf696aeccaa7410446a7b3dbf56533971
Phi Ha <phiha@Phis-iMac.local>
2023-04-01 21:21:55 +0700

    commit 01/04/2023-2

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,11 +1,40 @@
-import classNames from 'classnames/bind';
+import classNames from "classnames/bind";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleXmark,
+  faSpinner,
+  faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
 
-import styles from './Header.module.scss'
+import styles from "./Header.module.scss";
+import images from "~/assets/images";
 
-const cx = classNames.bind(styles)
+const cx = classNames.bind(styles);
 
 function Header() {
-    return <header className = {cx ('Wrapper')}></header>;
+  return (
+    <header className={cx("wrapper")}>
+      <div className={cx("inner")}>
+        <div className={cx("logo")}>
+          <img src={images.logo} alt="Tiktok" />
+        </div>
+
+        <div className={cx("search")} style={styles}>
+          <input placeholder="Search accounts and videos" spellCheck={false} />
+          <button className={cx("clear")}>
+            <FontAwesomeIcon icon={faCircleXmark} />
+          </button>
+          <FontAwesomeIcon className={cx("loading")} icon={faSpinner} />
+
+          <button className={cx("search-btn")} onClick={() => {}}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+        </div>
+
+        <div className={cx('actions')} ></div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
